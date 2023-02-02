17da157954db0b472d035cdb879f8c8c06ab37a0
Aleksandar <aleksandarichev@github.com>
2023-02-02 09:56:58 +0100

    user profile template added

---
+++ src/app/features/user-profile/user-profile.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserProfileRoutingModule } from './user-profile-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@@shared/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { UserProfileService } from './service/user-profile.service';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+@NgModule({
+	declarations: [UserProfileComponent],
+	imports: [
+		CommonModule,
+		UserProfileRoutingModule,
+		ReactiveFormsModule,
+		MaterialModule,
+		FlexLayoutModule,
+	],
+	providers: [UserProfileService],
+	exports: [UserProfileComponent],
+})
+export class UserProfileModule {}
